9a2eb65f5624f83a4caf47f77312cbefd736d273
antonella <antonella.gutierrezrodriguez@pcc.edu>
2019-11-07 21:11:14 -0800

    done with CSS

--- src/components/services/getAlbums.js
+++ src/components/services/getAlbums.js
@@ -1,5 +1,5 @@
 const getAlbums = (id, page) => {
-  return fetch(`http://musicbrainz.org/ws/2/release?artist=${id}&fmt=json&limit=10&offset=${page * 10}`)
+  return fetch(`http://musicbrainz.org/ws/2/release?artist=${id}&fmt=json&limit=4&offset=${page * 4}`)
     .then(res => res.json())
     .then(results => results.releases.map(({ id, title }) => ({
       id,
--- src/components/services/getArtists.js
+++ src/components/services/getArtists.js
@@ -1,5 +1,5 @@
 const getArtists = (name, page) => {
-  return fetch(`http://musicbrainz.org/ws/2/artist?query=${name}&fmt=json&limit=10&offset=${10 * page}`)
+  return fetch(`http://musicbrainz.org/ws/2/artist?query=${name}&fmt=json&limit=3&offset=${3 * page}`)
     .then(res => res.json())
     .then(results => results.artists.map(({ id, name, country, disambiguation }) => ({
       id,
